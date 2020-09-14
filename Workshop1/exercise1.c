0abe1f16c801810ff7e006e8ef8c977dfa3d604e
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2020-09-14 13:14:52 -0600

    [Update] - First exercise was finished

--- Workshop1/exercise1.c
+++ Workshop1/exercise1.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 
 
-#define MAX_ARRAY_SIZE 10000
+#define MAX_ARRAY_SIZE 32
 // 10 000us = 10ms
 #define THREAD1_TIME 10000
 // 5 000us = 5ms
@@ -13,23 +13,36 @@
 
 
 /**
- * Struct to represent the Pthread arguments
+ * Struct to represent the Pthread arguments of the random number generator
+ * function.
  */
-struct PthreadArgs
+struct PthreadArgsRNG
 {
     int *array;
     int delay;
+};
+
+/**
+ * Struct to represent the Pthread arguments of the XOR function.
+ */
+struct PthreadArgsXOR
+{
+    int *array1;
+    int *array2;
 };
 
 
 /**
  * This function generates a 8-bit random number and store it in an
  * array every 10 ms.
+ * 
+ * Params:
+ *      - PthreadArgsRNG struct pointer as a void pointer.
  */
 void generate_random_numbers(void* args)
 {
     // Get args
-    struct PthreadArgs pthread_args = *(struct PthreadArgs*) args;
+    struct PthreadArgsRNG pthread_args = *(struct PthreadArgsRNG*) args;
     int* array = pthread_args.array;
     int delay = pthread_args.delay;
 
@@ -38,17 +51,40 @@ void generate_random_numbers(void* args)
 
     int i = 0;
 
-    while (i < MAX_ARRAY_SIZE)
+    while (1)
     {
         // Generating a 8-bit random number
-        array[i] = rand() % 255;
-        i++;
+        array[i] = rand() % 256;
+        // Restart if i is equal to MAX_ARRAY_SIZE otherwise go to the next 
+        i = i == MAX_ARRAY_SIZE ? 0 : i + 1;
 
         // Sleeping 10ms
         usleep(delay);
-
-        printf("Num %d: %d\n", i - 1, array[i - 1]);
     }
+}
+
+/**
+ *
+ */
+void do_xor(void* args)
+{
+    // Get args
+    struct PthreadArgsXOR pthread_ags = *(struct PthreadArgsXOR*) args;
+    int* array1 = pthread_ags.array1;
+    int* array2 = pthread_ags.array2;
+
+    int i = 0;
+
+    while (1)
+    {
+        const int xor = array1[i] ^ array2[i];
+        // Restart if i is equal to MAX_ARRAY_SIZE otherwise go to the next 
+        i = i == MAX_ARRAY_SIZE ? 0 : i + 1;
+
+        // Display char
+        printf("%c\n", xor);
+    }
+    
 }
 
 
@@ -60,19 +96,25 @@ int main(int argc, char const *argv[])
 
     // Pthreads
     pthread_t thread_id1, thread_id2, thread_id3;
-    // Function arguments
-    struct PthreadArgs args1 = {array1, THREAD1_TIME};
-    struct PthreadArgs args2 = {array2, THREAD2_TIME};
 
-    // Creates threads to generate random numbers
+    // Functions arguments
+    struct PthreadArgsRNG args1 = {array1, THREAD1_TIME};
+    struct PthreadArgsRNG args2 = {array2, THREAD2_TIME};
+    struct PthreadArgsXOR args3 = {array1, array2};
+
+    // Threads to generate random numbers in different times
     pthread_create(&thread_id1, NULL, (void*) generate_random_numbers,
                    (void*) &args1);
     pthread_create(&thread_id2, NULL, (void*) generate_random_numbers,
                    (void*) &args2);
+    // Thread to display every char resulting of the XOR of the two arrays
+    pthread_create(&thread_id3, NULL, (void*) do_xor,
+                   (void*) &args3);
 
     // Starting threads
     pthread_join(thread_id1, NULL);
     pthread_join(thread_id2, NULL);
+    pthread_join(thread_id3, NULL);
 
     // Free memory
     free(array1);
